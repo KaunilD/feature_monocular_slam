5407968afcbb726251acc0757a831874a85dfd56
Kaunil Dhruv <dhruv.kaunil@gmail.com>
2018-11-23 17:14:16 -0500

    refactored to frame

--- feature_extractor.py
+++ feature_extractor.py
@@ -5,90 +5,83 @@ from skimage.transform import FundamentalMatrixTransform
 from skimage.transform import EssentialMatrixTransform
 
 
-class FeatureExtractor:
-    def __init__(self, K):
-        self.orb = cv2.ORB_create()
-        self.last = None
-        self.bf_matcher = cv2.BFMatcher(
-            cv2.NORM_HAMMING
-        )
+def extract(image):
+    orb = cv2.ORB_create()
+    features = cv2.goodFeaturesToTrack(
+                    image,
+                    qualityLevel = 0.01,
+                    maxCorners = 3000,
+                    minDistance = 3
+                )
+
+    keypoints = [
+            cv2.KeyPoint(
+                x = feature[0][0],
+                y = feature[0][1],
+                _size = 30
+            ) for feature in features
+    ]
+    keypoints, descriptors = orb.compute(image, keypoints)
+    features = np.array([(keypoint.pt[0], keypoint.pt[1]) for keypoint in keypoints])
+    return features, descriptors
+
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis = 1)
+
+def normalize(Kinv, point):
+    return np.dot(Kinv, add_ones(point).T).T[:, 0:2]
+
+def denormalize(K, point):
+    denorm = np.dot(K, [point[0], point[1], 1.0])
+    denorm/=denorm[2]
+    return int(round(denorm[0])), int(round(denorm[1]))
+
+def match(f1, f2):
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+    matches = bf.knnMatch(f1.des, f2.des, k=2)
+    ret = []
+    for m, n in matches:
+        if m.distance < 0.75*n.distance:
+            keypoint_1 = f1.points[m.queryIdx]
+            keypoint_2 = f2.points[m.trainIdx]
+            ret.append((keypoint_1, keypoint_2))
+    assert len(ret) > 8
+    ret = np.array(ret)
+    model, inliers = ransac(
+                        (ret[:, 0], ret[:, 1]),
+                        FundamentalMatrixTransform,
+                        min_samples = 8, residual_threshold  = 3,
+                        max_trials = 100
+                    )
+    ret = ret[inliers]
+
+    R, t = calc_pose_matrices(model)
+
+    # print('FEATUREEXTRACTOR: matches: {}'.format(len(ret)))
+    return ret, (R, t)
+
+def calc_pose_matrices(model):
+    W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=np.float)
+    U, w, V = np.linalg.svd(model.params)
+
+    assert np.linalg.det(U) > 0
+
+    if np.linalg.det(V) < 0:
+        V *= -1.0
+
+    R = np.dot(np.dot(U, W), V)
+
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), V)
+    t = U[:, 2]
+    return R, t
+
+class Frame(object):
+    def __init__(self, img, K):
 
         self.K = K
-        self.Kinv = np.linalg.inv(K)
-        print('FeatureExtractor: initialized feature extractor')
+        self.Kinv = np.linalg.inv(self.K)
 
-    def add_ones(self, x):
-        return np.concatenate([x, np.ones((x.shape[0], 1))], axis = 1)
-
-    def normalize(self, point):
-        return np.dot(self.Kinv, self.add_ones(point).T).T[:, 0:2]
-
-    def denormalize(self, point):
-        denorm = np.dot(self.K, [point[0], point[1], 1.0])
-        return int(round(denorm[0])), int(round(denorm[1]))
-
-    def extract(self, image):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        features = cv2.goodFeaturesToTrack(
-                        image,
-                        qualityLevel = 0.01,
-                        maxCorners = 3000,
-                        minDistance = 3
-                    )
-
-        keypoints = [
-                cv2.KeyPoint(
-                    x = feature[0][0],
-                    y = feature[0][1],
-                    _size = 30
-                ) for feature in features
-        ]
-        keypoints, descriptors = self.orb.compute(image, keypoints)
-
-        ret = []
-        R, t = None, None
-        if self.last is not None:
-            matches = self.bf_matcher.knnMatch(
-                        descriptors,
-                        self.last['descriptors'], k = 2
-                    )
-            for m, n in matches:
-                if m.distance < 0.75*n.distance:
-                    keypoint_1 = keypoints[m.queryIdx].pt
-                    keypoint_2 = self.last['keypoints'][m.trainIdx].pt
-                    ret.append((keypoint_1, keypoint_2))
-
-        self.last = {'keypoints': keypoints, 'descriptors': descriptors}
-        if len(ret) > 0:
-            ret = np.array(ret)
-            ret[:, 0, :] = self.normalize(ret[:, 0, :])
-            ret[:, 1, :] = self.normalize(ret[:, 1, :])
-            model, inliers = ransac(
-                                (ret[:, 0], ret[:, 1]),
-                                FundamentalMatrixTransform,
-                                min_samples = 8, residual_threshold  = 3,
-                                max_trials = 100
-                            )
-            ret = ret[inliers]
-
-            R, t = self.calc_pose_matrices(model)
-
-        # print('FEATUREEXTRACTOR: matches: {}'.format(len(ret)))
-        return ret, (R, t)
-
-    def calc_pose_matrices(self, model):
-        W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=np.float)
-        U, w, V = np.linalg.svd(model.params)
-
-        assert np.linalg.det(U) > 0
-
-        if np.linalg.det(V) < 0:
-            V *= -1.0
-
-        R = np.dot(np.dot(U, W), V)
-
-        if np.sum(R.diagonal()) < 0:
-            R = np.dot(np.dot(U, W.T), V)
-        t = U[:, 2]
-        return R, t
+        points, self.des = extract(img)
+        self.points = normalize(self.Kinv, points)
--- main.py
+++ main.py
@@ -5,43 +5,46 @@ sys.path.append('lib/')
 import g2o
 import numpy as np
 import cv2
-from feature_extractor import FeatureExtractor
+from feature_extractor import Frame, denormalize, match
 
 
 MP4 = 'data/videos/1.mp4'
-
-ROWS = 1920//2
-COLS = 1080//2
+W, H = 1920//2, 1080//2
 F = 1
-K = np.array(([F, 0, ROWS], [0, F, COLS], [0, 0, 1]))
+K = np.array(([F, 0, W], [0, F, H], [0, 0, 1]))
 
-def process_frame(frame, feature_extractor):
-    frame = cv2.resize(frame, (ROWS, COLS))
-    matches, (R, t) = feature_extractor.extract(frame)
-    if len(matches) == 0:
-        return frame
-    for p1, p2 in matches:
-        u1, v1 = feature_extractor.denormalize(p1)
-        u2, v2 = feature_extractor.denormalize(p2)
+frames = []
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    frame = Frame(img_gray, K)
+    frames.append(frame)
+
+    if len(frames) <= 1:
+        return img
+
+    ret, Rt = match(frames[-1], frames[-2])
+    for p1, p2 in ret:
+        u1, v1 = denormalize(K, p1)
+        u2, v2 = denormalize(K, p2)
         cv2.circle(
-            frame, (u1, v1), color=(0, 0, 255), radius = 2
+            img, (u1, v1), color=(0, 0, 255), radius = 2
+        )
+        cv2.line(
+            img, (u1, v1), (u2, v2), color=(0, 255, 0)
         )
 
-        cv2.line(
-            frame, (u1, v1), (u2, v2), color=(0, 255, 0)
-        )
-    # print(R, t)
-    return frame
+    return img
 
 if __name__ == '__main__':
     frame_idx = 0
     video_cap = cv2.VideoCapture(MP4)
-    feature_extractor = FeatureExtractor(K)
 
     while video_cap.isOpened():
         ret, frame = video_cap.read()
         if ret:
-            frame = process_frame(frame, feature_extractor)
+            frame = process_frame(frame)
             frame_idx += 1
         else:
             break
